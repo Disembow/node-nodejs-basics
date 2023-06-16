d2df8ce641874d82c8374da1de26b164085d4350
Disembow <e_naletov@mail.ru>
2023-06-16 11:01:14 +0200

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+const { createHash } = await import('node:crypto');
+import { readFile } from 'node:fs/promises';
+import path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const pathToFile = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  try {
+    const result = await readFile(pathToFile, 'utf-8');
+    hash.update(result);
+    console.log(hash.digest('hex'));
+  } catch (error) {
+    console.error(error);
+  }
+
+  hash.end();
 };
 
 await calculateHash();
