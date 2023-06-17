e20be79903c4b8b72b9f92f36eaa0f1f919f517d
Disembow <e_naletov@mail.ru>
2023-06-17 14:33:31 +0200

    feat: implement zip part

--- src/streams/read.js
+++ src/streams/read.js
@@ -4,10 +4,10 @@ import { pipeline } from 'node:stream/promises';
 import * as url from 'url';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const pathtoFile = path.resolve(__dirname, 'files', 'fileToRead.txt');
+const pathToFile = path.resolve(__dirname, 'files', 'fileToRead.txt');
 
 const read = async () => {
-  const rs = createReadStream(pathtoFile);
+  const rs = createReadStream(pathToFile);
 
   await pipeline(rs, process.stdout);
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -5,10 +5,10 @@ import { pipeline } from 'node:stream/promises';
 import * as url from 'url';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const pathtoFile = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+const pathToFile = path.resolve(__dirname, 'files', 'fileToWrite.txt');
 
 const write = async () => {
-  const ws = createWriteStream(pathtoFile);
+  const ws = createWriteStream(pathToFile);
 
   await pipeline(process.stdin, ws);
 };
