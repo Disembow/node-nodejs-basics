1c247ced79940be528b850da22e0a96007f9a7f1
Disembow <e_naletov@mail.ru>
2023-06-19 13:33:40 +0200

    fix: add separator into output of args.js func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,10 +1,10 @@
-const parseArgs = () => {
+const parseArgs = () =>
   console.log(
     process.argv
       .slice(2)
       .map((e, i, a) => ((i + 1) % 2 === 0 ? `${a[i - 1]} is ${e}` : ''))
-      .join('')
+      .filter((e) => e !== '')
+      .join(', ')
   );
-};
 
 parseArgs();
